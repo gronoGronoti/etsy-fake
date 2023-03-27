ac23559e04123c27f870782553e91793c1ee7275
gronoGronoti <74327469+gronoGronoti@users.noreply.github.com>
2023-03-28 00:24:44 +0200

    imposto il main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material'
+import Router from './components/consts/Router'
+
 import './index.css'
+import MainTheme from './MainTheme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={MainTheme}>
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
