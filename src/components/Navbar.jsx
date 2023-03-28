03c17af0b0e7d995d6d58142f43bae5f1aff1e03
gronoGronoti <renysici2@gmail.com>
2023-03-28 11:05:47 +0200

    piccolo fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,33 +12,6 @@ import SearchBar from "./SearchBar/SearchBar";
 import AccessBar from "./AccessBar/AccessBar";
 import { BottomSecNavLinks } from "./consts/NavbarLinks";
 
-const TabPanel = (props) => {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-const a11yProps = (index) => {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
 const Navbar = () => {
   return (
     <>
