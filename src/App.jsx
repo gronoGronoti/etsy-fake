3d3e270dfdd32f1943bfdb2e2b0b3855b76f90f7
gronoGronoti <renysici2@gmail.com>
2023-03-28 11:56:14 +0200

    boh titolo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import './App.css'
-import Typography from '@mui/material/Typography'
+import React from "react";
+import "./App.css";
+import Typography from "@mui/material/Typography";
 
 const App = () => {
   return (
     <>
-    <div className='beige-banner'>App</div>
+      <div className="beige-banner">
+        <Typography variant="h4" sx={{ textAlign: "center", pt: 3 }}>
+          Scopri articoli da amare. Sostieni negozi indipendenti. Solo su
+          Stepepetsy.
+        </Typography>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
