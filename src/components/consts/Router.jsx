9c8a957f8c54c63c13157d1f7824f241f02125a4
gronoGronoti <renysici2@gmail.com>
2023-03-28 09:57:12 +0200

    fix routing

--- src/components/consts/Router.jsx
+++ src/components/consts/Router.jsx
@@ -16,7 +16,7 @@ const Router = () => {
           element: <App />,
         },
         {
-          path: "not-found",
+          path: "*",
           element: <NotFound />,
         },
       ],
